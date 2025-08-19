0fb2de52dd364e1a9ee9acc85ebfb0b0f721d8c6
Gfajar1 <gfajar1@lsu.edu>
2025-08-19 11:41:59 -0600

    Another project

---
+++ MathMagic/Magic.java
@@ -0,0 +1,24 @@
+public class Magic {
+	public static void main(String[] args) {
+    //myNumber is the original number.
+
+ // Uncomment the following block to see each step of the magic trick
+  /*
+    //myNumber is the original number.
+    int myNumber = 4;
+    int stepOne = myNumber * myNumber;
+    int stepTwo = stepOne + myNumber;
+    int stepThree = stepTwo / myNumber;
+    int stepFour = stepThree + 17;
+    int stepFive = stepFour - myNumber;
+    int stepSix = stepFive / 6;
+
+    System.out.println(stepSix);
+	*/
+    int myNumber = 10;
+    // magicNumber calculates the magic number using the math trick formula
+    int magicNumber = ((((((myNumber * myNumber) + myNumber) / myNumber) + 17) - myNumber) / 6);
+
+    System.out.println(magicNumber);
+	}
+}
