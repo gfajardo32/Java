a43b4d0ee5af4aac6584162687fbee0808264e3c
Guido Fajardo <guidomanfajardo@gmail.com>
2025-08-26 17:23:38 -0500

    Refactor comments and formatting in Droid.java for clarity and consistency

--- droidproject/Droid.java
+++ droidproject/Droid.java
@@ -1,13 +1,24 @@
 package droidproject;
+
+/*this is a project that builds a droid with certain attributes and behaviors
+*the droid can perform tasks that drain its battery level also it can report its battery level
+*the droid can only perform a task if it has enough battery level 
+*author: Guido Fajardo
+*date: 08/26/2025
+*/
+
+//Create a class named Droid
 public class Droid {
   //State
   public String name;
   public int batteryLevel = 100;
 
+    //Constructor
   @Override
   public String toString() {
     return "Hello I'm the droid: " + name + "!";
   }
+    //Constructor method
   public Droid(String droidName) {
     name = droidName;
   }
@@ -21,14 +32,15 @@ public class Droid {
     }
   }
 
+  //Method that returns the battery level
   public String stateBatteryLevel(){
     return "The battery level is at " + this.batteryLevel + "%";
   }
-
+ // Method that prints the battery level
   public void energyReport() {
     System.out.println(this.name + " has " + this.batteryLevel + "% of battery left!");
   }
-
+ // Main method
   public static void main(String[] args) {
     Droid Codey = new Droid("Codey");
 
